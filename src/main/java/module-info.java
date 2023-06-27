ea3735d12e9b271d3532ec2d96b1ed16a125d73d
Hwanyi <god38111@tobenix.co.kr>
2023-06-27 13:37:17 +0900

    HelloController 및 HelloControllerTest 작성

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.jojodu.book.freelecspringboot2webservice {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.jojodu.book.freelecspringboot2webservice to javafx.fxml;
-    exports com.jojodu.book.freelecspringboot2webservice;
-}
