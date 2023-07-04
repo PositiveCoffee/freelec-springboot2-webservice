c7cc44ef7918f736f0504088509a16444751815f
Hwanyi <god38111@tobenix.co.kr>
2023-07-04 10:49:35 +0900

    세션에서 로그인유저를 불러오는 메소드를 어노테이션 @LoginUser로 대체

---
+++ src/main/java/com/jojoldu/book/springboot/config/auth/LoginUser.java
@@ -0,0 +1,11 @@
+package com.jojoldu.book.springboot.config.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER) //파라미터 타입
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser { // 어노테이션
+}
